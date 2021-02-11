4ec42d0f06c63ffaf1c91631d74c8b58a366b8a9
heyiamfalloutgirl <69209863+heyiamfalloutgirl@users.noreply.github.com>
2021-02-11 17:07:30 +0300

    C++_laboratory_work2

--- Header.h
+++ Header.h
@@ -1,191 +1,42 @@
 #pragma once
-#define N 20
+#include <string>
 
 using namespace std;
 
-// Пользовательский класс Pair (пара чисел)
-class Pair{
-private:
-	int val1;
-	double val2;
+typedef struct{ // Стукрута для даты полета
+	int day;
+	int month;
+	int year;
+} DATE;
+
+typedef struct{ // Стукрута для ФИО
+	string f;
+	string i;
+	string o;
+} FIO;
+
+class Passenger{ // Класс данных пассажира
 public:
-	Pair() : val1(0), val2(0.0) {}
-	Pair(const int &a, const double &b) : val1(a), val2(b) { } // Конструктор по заданным значениям
-	~Pair() {}
-	void Show(){ // Вывод на экран
-		cout << val1 << ":" << val2 << endl;
+	int flightnum; // Номер рейса
+	DATE date; 
+	FIO fio;
+	int seatnum; // Номер места
+
+	Passenger() { // Конструктор
 	}
-	friend ostream &operator<<(ostream &os, const Pair &p){ // Дружественная функция перегрузки оператора вывода
-		os << p.val1 << ":" << p.val2;
-		return os;
-	}
-	friend istream &operator>>(istream &is, Pair &p){ // Дружественная функция перегрузки оператора ввода
-		cout << "Введите пару чисел (int и double): ";
-		is >> p.val1;
-		is >> p.val2;
-		return is;
-	}
-	const Pair &operator=(const Pair &p){ // Перегрузка оператора присваивания
-		val1 = p.val1;
-		val2 = p.val2;
-		return *this;
+	~Passenger() { //Деструктор
 	}
 
 	// Перегрузка операторов равенств и сравнения:
-	bool operator==(const Pair &p) const{
-		return (val1 == p.val1 and val2 == p.val2); 
-	}
-	bool operator!=(const Pair &p) const{
-		return !(*this == p); // Перегружаем с помощью логической противоположности
-	}
-	bool operator>=(const Pair &p) const{
-		if (val1 > p.val1) // Рассматриваем по 1м цифрам из пары
-			return true;
-		else if (val1 == p.val1) // Если они равны - смотрим вторые
-			if (val2 > p.val2)
-				return true;
-		return false;
-	}
-	bool operator>(const Pair &p) const{
-		return ((*this >= p) && (*this != p)); // >= и != дает >
-	}
-	bool operator<(const Pair &p) const{
-		return !(*this >= p); // !(>=) дает <
-	}
-	bool operator<=(const Pair &p) const{
-		return (!(*this >= p) || *this == p); // !(>=) или == дает < или == (<=)
-	}
+	bool operator >= (const Passenger&) const;
+	bool operator > (const Passenger&) const;
+	bool operator < (const Passenger&) const;
+	bool operator <= (const Passenger&) const;
+	bool operator != (const Passenger&) const;
+	bool operator == (const Passenger&) const;
 };
 
-
-// Шаблон класса-контейнера (множество)
-template <class Type>
-class Set{ // Множество
-private:
-	Type arr[N];
-	int size = 0; // Количество элементов множества
-public:
-	Set() {}
-
-	Set(const Type *a, int length){ // Конструктор по переданному массиву
-		size = length; // Запоминаем длину = кол-во элементов
-		for (int i = 0; i < size; i++) // Поэлементно присваиваем значения
-			arr[i] = a[i];
-	}
-
-	~Set() {}
-
-	const Set<Type> &operator=(const Set<Type> &set){ // Перегрузка оператора присваивания
-		size = set.size;
-		for (int i = 0; i < size; i++)
-			arr[i] = set.arr[i]; // Поэлементно присваиваем значения
-		set.insertSort(); // Сортируем элементы множества
-		return *this;
-	}
-
-	Set(const Set<Type> &set) { // Конструктор копирования
-		*this = set; // Перегруженный оператор присваивания!
-	}
-
-	int ShowSet(){ // Вывод на экран
-		if (size == 0){ // Проверка на пустое множество
-			cout << "Set is empty :(" << endl;
-			return 1;
-		}
-		cout << "{";
-		for (int i = 0; i < size - 1; i++)
-			cout << arr[i] << ", ";
-		cout << arr[size - 1];
-		cout << "}" << endl;
-		return 0;
-	}
-
-	friend ostream &operator<<(ostream &os, Set<Type> &set){ // Перегрузка оператора вывода
-		set.insertSort(); // Сортируем элементы множества перед выводом
-		set.ShowSet();
-		return os;
-	}
-
-	friend istream &operator>>(istream &is, Set<Type> &set){ // Перегрузка оператора ввода
-		Type a;
-		int n;
-		cout << "Введите кол-во элементов множества: ";
-		cin >> n;
-		cout << "Введите эти элементы:" << endl;
-		for (int i = 0; i < n; i++){ // В цикле вводим элемент а типа Type и добавляем его во множество
-			is >> a; 
-			set.add(a);
-		};
-		set.insertSort();  // Сортируем элементы множества после ввода
-		return is;
-	}
-
-	bool IsElmInSet(const Type &value){ // Проверка принадлежности множеству
-		for (int i = 0; i < size; i++) // Проходим по циклу, сравниваем все элементы с заданным
-			if (value == arr[i])
-				return true;
-		return false;
-	}
-
-	int add(const Type &value){ // Добавление элемента во множество
-		if (size >= N){ // Проверка на выход за пределы множества
-			cout << "No space in Set!" << endl;
-			return 1;
-		}
-		if (IsElmInSet(value)){ // Проверка есть ли уже во множестве такой элемент
-			cout << "This element is already exist in Set!" << endl;
-			return 1;
-		}
-		arr[size] = value; // Добавляем элемент во множество
-		size++;
-		cout << "Element " << value << " is added." << std::endl;
-		return 0;
-	}
-
-	void del(const Type &value){ // Удаление элемента из множества
-		if (IsElmInSet(value)){ // Проверка есть ли во множестве такой элемент
-			int i;
-			for (i = 0; i < size; i++)
-				if (value == arr[i]) // Доходим до элемента, который нужно удалить
-					break;
-			for (int j = i; j < size; j++) // Сдвигаем все элементы нна место удаленного
-				arr[j] = arr[j + 1];
-			--size; // Уменьшаем размер множества
-			cout << "Element " << value << " is deleted." << endl;
-		}
-		else
-			cout << "There is no " << value << " in this Set!" << endl;
-	}
-
-	void getsize() const{ // Определение размера множества
-		cout << "Size of set: " << size << endl; // Выводим кол-во элементов множества
-	}
-
-	int IsEmpty(){ // Проверка на пустоту
-		if (size != 0){ // Определяем по размеру 
-			cout << "Set isn't empty." << endl;
-			return 0;
-		}
-		else{
-			cout << "Set is empty." << endl;
-			return 1;
-		}
-	}
-
-	// Сортировка вставками по возрастанию для элементов множества
-	void insertSort()
-	{
-		Type s; // Переменная для хранения значения элемента сортируемого массива
-		int	j; // Индекс предыдущего элемента
-		for (int i = 1; i < size; i++)
-		{
-			s = arr[i]; // Инициализируем временную переменную текущим значением элемента массива
-			j = i - 1; // Запоминаем индекс предыдущего элемента
-			while (j >= 0 && arr[j] > s){ // Пока индекс != 0 и предыдущий элемент массива > текущего
-				arr[j + 1] = arr[j]; // Меняем элементы местами
-				arr[j] = s;
-				j--;
-			}
-		}
-	}
-};
+// Прототивы функций
+Passenger *bubbleSort(Passenger *ps, int n); // Сортировка пузырьком
+Passenger *insertSort(Passenger *ps, int n); // Сортировка вставками
+void print(Passenger *ps, int n); // Печать массива
---
+++ Source1.cpp
@@ -0,0 +1,136 @@
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "Header.h"
+
+using namespace std;
+
+// Перегрузка оператора ==
+bool Passenger::operator ==(const Passenger &ps)const
+{
+	return !(*this != ps);
+}
+
+// Перегрузка оператора !=
+bool Passenger::operator !=(const Passenger &ps)const
+{
+	if (fio.f != ps.fio.f) // Рассматриваем равенство для всех полей
+		return true;
+	if (fio.i != ps.fio.i)
+		return true;
+	if (fio.o != ps.fio.o)
+		return true;
+	if (date.day != ps.date.day)
+		return true;
+	if (date.month != ps.date.month)
+		return true;
+	if (date.year != ps.date.year)
+		return true;
+	if (flightnum != ps.flightnum)
+		return true;
+	if (seatnum != ps.seatnum)
+		return true;
+	return false;
+}
+
+// Перегрузка оператора >=
+bool Passenger::operator >=(const Passenger &ps)const
+{
+	if (date.year > ps.date.year) // Сравниваем все поля в требуемом приоритете: дата.год рейса
+		return true;
+	else if (date.year == ps.date.year)
+		if (date.month > ps.date.month) // дата.месяц рейса
+			return true;
+		else if (date.month == ps.date.month)
+			if (date.day > ps.date.day) // дата.день рейса
+				return true;
+			else if (date.day == ps.date.day)
+				if (flightnum > ps.flightnum) // номер рейса
+					return true;
+				else if (flightnum == ps.flightnum)
+					if (fio.f > ps.fio.f) // фамилия пассажира
+						return true;
+					else if (fio.f == ps.fio.f)
+						if (fio.i > ps.fio.i) // имя пассажира
+							return true;
+						else if (fio.i == ps.fio.i)
+							if (fio.o > ps.fio.o) // отчество пассажира
+								return true;
+	return false;
+}
+
+// Перегрузка оператора <
+bool Passenger::operator <(const Passenger &ps)const
+{
+	return !(*this >= ps); // Перегружаем с помощью логической противоположности: !(>=) дает <
+}
+
+// Перегрузка оператора >
+bool Passenger::operator >(const Passenger &ps)const
+{
+	if ((*this >= ps) && (*this != ps)) // Перегружаем с помощью логических противоположностей: >= и != дает >
+		return true;
+	return false;
+}
+
+//Перегрузка оператора <=
+bool Passenger::operator <=(const Passenger &ps)const
+{
+	if (!(*this >= ps) || *this == ps) // Перегружаем с помощью логических противоположностей: !(>=) или == дает < или ==
+		return true;
+	return false;
+}
+
+// Сортировка пузырьком по возрастанию (https://codelessons.ru/cplusplus/algoritmy/puzyrkovaya-sortirovka-v-c-glavnye-momenty.html)
+Passenger *bubbleSort(Passenger *ps, int n) // Передаем в нее массив пассажиров и кол-во элементов массива
+{
+	Passenger s; // Переменная для обмена элементов местами
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = 0; j < n - i - 1; j++)
+		{
+			if (ps[j] > ps[j + 1])
+			{
+				// Меняем элементы местами
+				s = ps[j];
+				ps[j] = ps[j + 1];
+				ps[j + 1] = s;
+			}
+		}
+	}
+
+	return ps;
+}
+
+// Сортировка вставками по возрастанию (http://cppstudio.com/post/462/)
+Passenger *insertSort(Passenger *ps, int n) 
+{
+	Passenger s; // Переменная для хранения значения элемента сортируемого массива
+	int	j; // Индекс предыдущего элемента
+
+	for (int i = 1; i < n; i++)
+	{
+		s = ps[i]; // Инициализируем временную переменную текущим значением элемента массива
+		j = i - 1; // Запоминаем индекс предыдущего элемента
+		while (j >= 0 && ps[j] > s) // Пока индекс != 0 и предыдущий элемент массива > текущего
+		{
+			ps[j + 1] = ps[j]; // Меняем элементы местами
+			ps[j] = s;
+			j--;
+		}
+	}
+
+	return ps;
+}
+
+// Печать массива
+void print(Passenger *ps, int n)
+{
+	cout << "flnum" << std::setfill(' ') << std::setw(9) << "dd mm yy" << std::setfill(' ') << std::setw(15)
+		<< "FIO" << std::setfill(' ') << std::setw(17) << "seatnum" << "\n"; // Обозначение столбцов
+	for (int i = 0; i < n; i++) // В цике печатаем все столбцы
+		cout << ps[i].flightnum << " " << ps[i].date.day << " " << ps[i].date.month << " " << ps[i].date.year
+		<< " " << ps[i].fio.f << " " << ps[i].fio.i << " " << ps[i].fio.o << " " << ps[i].seatnum << endl;
+}
